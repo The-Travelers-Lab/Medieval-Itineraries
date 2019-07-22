c27765afabb38f8bce973943578553d4a36271a9
Adam Franklin-Lyons <adamfl@marlboro.edu>
2019-07-21 20:36:57 -0400

    Add files via upload

--- Itinerary-Project-Code/interactive_gaz_itin_commands.py
+++ Itinerary-Project-Code/interactive_gaz_itin_commands.py
@@ -29,6 +29,7 @@ Created on Tue Jul  2 13:36:40 2019
 from gazetteer_class import Gazetteer
 from itinerary_class import Itinerary
 import os
+import sys
 import pandas as pd
 
 Command_Dict = {'process a gazetteer': 'run_gaz',
@@ -70,7 +71,9 @@ def main():
     """
     try:
         file_path = get_current_path("gazetteer_and_itinerary_functions_list.txt")
+        print(file_path)
         with open(file_path, "r") as input_file:
+            print(3)
             input_text = input_file.readlines()
             input_text = input_text[input_text.index('Gazetteer Functions:\n')+1:]
             choice_dict = {}
@@ -85,8 +88,7 @@ def main():
             itin_functions(choice_dict)
         print_choices(choice_dict)
     except OSError:
-        print('There is a problem with the functions filename '
-              'or the file is not in the same folder.  Please try again.')
+        print('One of your file names is not valid.  Please try again.')
 
 def function_names(line):
     choice = line.split(':')
@@ -157,8 +159,8 @@ def gaz_functions(choice_dict):
     else:
         main_gaz.csv_output(out_file_name=choice_dict['final_save'])
     if choice_dict['g_error_output'] == True:
-        main_gaz.error_output(tofile=True,
-                              filename=choice_dict['g_error_file'])
+        file_path = get_current_path(choice_dict['g_error_file'])
+        main_gaz.error_output(tofile=True, filename=file_path)
 
 def itin_functions(choice_dict):
     """
@@ -198,8 +200,7 @@ def itin_functions(choice_dict):
         main_itin.format_dates()
     if choice_dict['itin_to_gaz'] == True:
         itin_gaz_df = main_itin.itin_to_gaz()
-        output_path = get_current_path(choice_dict['gaz_file_out'])
-        itin_gaz_df.to_csv(output_path, index=False)
+        itin_gaz_df.to_csv(choice_dict['gaz_file_out'], index=False)
     if choice_dict['itin_to_trips'] == True:
         if choice_dict['keep_dates'] == True:
             itin_trips_df = main_itin.itin_to_trips(date_style='all')
@@ -208,17 +209,19 @@ def itin_functions(choice_dict):
         itin_trips_df.to_csv(choice_dict['trips_file'], index=False)
     if choice_dict['final_itin_save'] == 'same':
         filename = (main_itin.name + '_processed.csv')
-        main_itin.itin_df.to_csv(filename, index=False)
+        itin_out_path = get_current_path(filename)
+        main_itin.itin_df.to_csv(itin_out_path, index=False)
     elif choice_dict['final_itin_save'] == 'none':
         None
     else:
-        main_itin.itin_df.to_csv(choice_dict['final_itin_save'], index=False)
+        itin_out_path = get_current_path(choice_dict['final_itin_save'])
+        main_itin.itin_df.to_csv(itin_out_path, index=False)
     if choice_dict['i_error_output'] == True:
-        main_itin.error_output(tofile=True,
-                              filename=choice_dict['i_error_file'])
+        file_path = get_current_path(choice_dict['i_error_file'])
+        main_itin.error_output(tofile=True, filename=file_path)
 
 def get_current_path(filename):
-    file_dir = os.path.dirname(os.path.realpath('__file__'))
+    file_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
     file_path = os.path.join(file_dir, filename)
     return file_path
 
